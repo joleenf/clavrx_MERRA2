abc18827813b24abd64b01e6e55fe6746938d931
Joleen Feltz <joleenf@ssec.wisc.edu>
2021-12-28 12:39:26 -0600

    Changes to make this work the same on both older and newer versions of bash  - Remove associative array since this seems to be available only on later bash  - Update how inpath is used so that paths are set correct for both temporary    directory and fixed directory.

--- scripts/wget_all.sh
+++ scripts/wget_all.sh
@@ -21,16 +21,13 @@ EndOfMessage
 }
 
 download_dir=$(pwd)
-key=all
-declare -a FILETYPES=([inst6_3d_ana_Np]=3d_ana [inst6_3d_ana_Nv]=3d_ana [tavg1_2d_slv_Nx]=2d_slv \
-	               [tavg1_2d_flx_Nx]=2d_flx [inst3_3d_asm_Np]=3d_asm [const_2d_ctm_Nx]=2d_ctm \
-		       [inst1_2d_asm_Nx]=2d_asm [tavg1_2d_lnd_Nx]=2d_lnd [tavg1_2d_rad_Nx]=2d_rad) 
+in_key=all
 
 [ $# -eq 0 ] && usage
 while getopts "w:k:h" flag; do
 	case "$flag" in 
 		w) download_dir=$OPTARG;;
-		k) key=$OPTARG;;
+		k) in_key=$OPTARG;;
 		h) usage;;
 	esac
 done
@@ -50,12 +47,40 @@ else
 	exit 1
 fi
 
-[ "${key}" == "all" ] && this_set=${!FILETYPES[@]} || this_set=${key}
-echo ${this_set}
-for file_key in "${!FILETYPES[@]}"
+case "${in_key}" in
+   all) FILETYPES=(inst6_3d_ana_Np::3d_ana inst6_3d_ana_Nv::3d_ana tavg1_2d_slv_Nx::2d_slv
+                   tavg1_2d_flx_Nx::2d_flx inst3_3d_asm_Np::3d_asm const_2d_ctm_Nx::2d_ctm
+                   inst1_2d_asm_Nx::2d_asm tavg1_2d_lnd_Nx::2d_lnd tavg1_2d_rad_Nx::2d_rad)
+                    ;;
+   inst6_3d_ana_Np) FILETYPES=(inst6_3d_ana_Np::3d_ana)
+                    ;;
+   inst6_3d_ana_Nv) FILETYPES=(inst6_3d_ana_Nv::3d_ana)
+                    ;;
+   tavg1_2d_slv_Nx) FILETYPES=(tavg1_2d_slv_Nx::2d_slv)
+                    ;;
+   tavg1_2d_flx_Nx) FILETYPES=(tavg1_2d_flx_Nx::2d_flx)
+                    ;;
+   inst3_3d_asm_Np) FILETYPES=(inst3_3d_asm_Np::3d_asm)
+                    ;;
+   const_2d_ctm_Nx) FILETYPES=(const_2d_ctm_Nx::2d_ctm)
+                    ;;
+   inst1_2d_asm_Nx) FILETYPES=(inst1_2d_asm_Nx::2d_asm)
+                    ;;
+   tavg1_2d_lnd_Nx) FILETYPES=(tavg1_2d_lnd_Nx::2d_lnd)
+                    ;;
+   tavg1_2d_rad_Nx) FILETYPES=(tavg1_2d_rad_Nx::2d_rad)
+                    ;;
+   *) echo "Unkown key $in_key"
+      exit 1
+      ;;
+esac
+
+for association in "${FILETYPES[@]}"
 do
-	echo "${file_key}"
-	mkdir -p ${FILETYPES[$file_key]}
-	${SCRIPTS_DIR}/wget_${file_key}.sh ${YYYY} ${MM} ${DD}
+	echo "${association}"
+        key="${association%%::*}"
+        value="${association##*::}"
+	mkdir -p ${value}
+	${SCRIPTS_DIR}/wget_${key}.sh ${YYYY} ${MM} ${DD}
 	cd ${download_dir}
 done
