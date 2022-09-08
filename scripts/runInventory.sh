692fbafc3270b3d10c802d6de1c29fd5e2aac3b4
clavrx utility <clavrx_ops@vor.ssec.wisc.edu>
2022-09-08 01:23:11 +0000

    Update option for current_year

--- scripts/runInventory.sh
+++ scripts/runInventory.sh
@@ -3,8 +3,9 @@
 # Run using screen -dm -S run_for_year /bin/bash runYear.sh <year>
 # Ex.) screen -dm -S run_2020 /bin/bash runYear.sh 2020
 
+curr_year=`date +"%Y"`
 BASE="$( cd -P "$( dirname "$0" )" && pwd )"
-YEAR=${1:-2021}
+YEAR=${1:-$curr_year}
 
 for i in {1..12}
 do
