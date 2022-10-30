201464fd9d0b52765faadbaba9951a39278316ab
clavrx utility <clavrx_ops@vor.ssec.wisc.edu>
2022-10-30 12:18:23 +0000

    Running Brett's version of the code  should not be listed as test.

--- runMonth.sh
+++ runMonth.sh
@@ -52,10 +52,10 @@ end_date=$(date -d $end_day +%Y%m%d)
 #screen -dm -S $screen_name /bin/bash $bin_dir/run_merra24.sh $start_day $end_day
 while [[ $start_date -le $end_date ]];
 do
-    /bin/bash $bin_dir/test_merra24clavrx_brett.sh $start_date 
+    /bin/bash $bin_dir/run_merra24clavrx_brett.sh $start_date 
     start_date=$(date -d"$start_date + 1 day" +"%Y%m%d")
 done
 
-echo "/bin/bash $bin_dir/test_merra24clavrx_brett.sh $start_day $end_day"
+echo "/bin/bash $bin_dir/run_merra24clavrx_brett.sh $start_day $end_day"
 sh $bin_dir/scripts/count_inventory.sh $YEAR $MONTH >> $LOG_DIR/inventory_${YEAR}.log
 exit
