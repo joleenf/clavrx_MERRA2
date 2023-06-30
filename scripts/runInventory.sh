033225fea38b6e7a57aa0c75e4ae74ae8b749ed9
clavrx utility <clavrx_ops@vor.ssec.wisc.edu>
2023-06-30 19:23:58 +0000

    Fix loop so that it only loops through to most recent month rather than months that have not yet appeared.

--- scripts/runInventory.sh
+++ scripts/runInventory.sh
@@ -4,8 +4,16 @@
 # Ex.) screen -dm -S run_2020 /bin/bash runYear.sh 2020
 
 curr_year=`date +"%Y"`
+curr_month=`date +"%m"`
 BASE="$( cd -P "$( dirname "$0" )" && pwd )"
 YEAR=${1:-$curr_year}
+
+if [[ "${YEAR}" == "${curr_year}" ]];
+then
+    end_loop=$curr_month
+else
+    end_loop=12
+fi
 
 re='^[0-9]+$'
 if ! [[ $YEAR =~ $re ]] ; then
@@ -17,8 +25,10 @@ fi
 # make sure directory exists...
 mkdir -p /apollo/cloud/Ancil_Data/clavrx_ancil_data/dynamic/merra2/$YEAR
 
-for i in {1..12}
+i=1
+while [[ $i -le $end_loop ]]
 do
    screen_name=run$YEAR_$i
    /bin/bash $BASE/count_inventory.sh $YEAR $i;err=$?
+   ((i++))
 done
