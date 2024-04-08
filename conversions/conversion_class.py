Update to add dependent variables using a method rather than carrying a dictionary of multiple classes which was more confusing.  Update some type annotations and fix the RH calculation so that the fill value is correct

conversions/conversion_class.py
@@ -93,62 +93,70 @@ class ReanalysisConversion:
 
     def __init__(
             self,
-            nc_dataset=None,
-            shortname=None,
+            file_tag=None,
+            data_array=None,
             out_name=None,
             out_units=None,
             ndims_out=None,
-            not_masked=True,
+            unmask=True,
             nan_fill=False,
             time_ind=None
     ) -> None:
         """Based on variable, adjust shape, apply fill and determine dtype."""
-        self.nc_dataset = nc_dataset
-        self.shortname = shortname
+        self.file_tag = file_tag
+        self.shortname = data_array.name
+        self.long_name = data_array.long_name
         self.out_name = out_name
         self.out_units = out_units
         self.ndims_out = ndims_out
 
-        self.fill = self._get_fill
-        self.data = self._get_data(not_masked, nan_fill, time_ind)
+        self.fill = self.get_fill(data_array)
+        self.data = self._get_data(data_array, out_name, unmask,
+                                   nan_fill, time_ind)
+        self.dependent = None  # add later when necessary
 
     def __repr__(self):
         """Report the name conversion when creating this object."""
         str_template = "Input name {} ==> Output Name: {}"
-
-        return str_template.format(self[self.shortname].long_name, self.out_name)
-
-    def __getitem__(self, item):
-        """Access data in the NetCDF dataset variable by variable key."""
-        return self.nc_dataset.variables[item]
+        return str_template.format(self.long_name, self.out_name)
 
     def out_name(self):
         """Return variable name as defined in the output file."""
         return self.out_name
 
-    @property
-    def _get_fill(self):
+    @staticmethod
+    def get_fill(data_array):
         """Get the fill value of this data."""
-        if "_FillValue" in self[self.shortname].ncattrs():
-            fill = self[self.shortname].getncattr("_FillValue")
-        elif "missing_value" in self[self.shortname].ncattrs():
-            fill = self[self.shortname].getncattr("missing_value")
+        if "_FillValue" in data_array.ncattrs():
+            fill = data_array.getncattr("_FillValue")
+        elif "missing_value" in data_array.ncattrs():
+            fill = data_array.getncattr("missing_value")
         else:
             fill = None
 
         return fill
 
-    def _get_data(self, not_masked, nan_fill, time_ind):
-        """Get data and based on dimensions reorder axes, truncate TOA, apply fill."""
-        data_var = self[self.shortname]
-        print(f"Getting data for {self.shortname}")
+    def _get_data(self, data_var, out_name: str, unmask: bool,
+                  nan_fill: bool, time_ind: int, toa2sfc=True):
+        """Get data and based on dimensions reorder axes, truncate TOA, apply fill.
+        :param data_var: working data array (SDS)
+        :param out_name:  output name of variable (usually different than NASA name)
+        :param unmask:  fill masked values (true except for dependent variables)
+        :param nan_fill:  (bool) apply fill after unmask
+        :param time_ind:  index for current time in data_var
+        :keyword toa2src: (bool) flip level top of atmosphere to surface
+            to match traditional code, make this false for dependent level variables.
+        """
+        shortname = data_var.name
+        print(f"Getting data for {shortname}")
+
         data = np.ma.getdata(data_var)
 
         # want only dependent arrays as masked arrays, everything else can be a regular np.array.
-        if not_masked:
+        if unmask:
             data = np.asarray(data.filled())
 
-        if self.shortname == "lev" and len(data) != 42:
+        if shortname == "lev" and len(data) != 42:
             # insurance policy while levels are hard-coded in unit conversion fn's
             # also is expected based on data documentation:
             # https://gmao.gsfc.nasa.gov/pubs/docs/Bosilovich785.pdf
@@ -161,15 +169,15 @@ class ReanalysisConversion:
             # note, vars w/ 3 spatial dims will be 4d due to time
             data = data[time_ind]
 
-        # apply special cases
-        if self.shortname in ("lev", "level"):
+        # # # apply special cases
+        if shortname in ("lev", "level") and toa2sfc:
             data = data.astype(np.float32)
             data = np.flipud(data)  # clavr-x needs toa->surface
-        elif self.shortname in ("lon", "longitude"):
-            data = self._reorder_lon(self.shortname, data)
+        elif shortname in ("lon", "longitude"):
+            data = self._reorder_lon(shortname, data)
 
-        if self.fill is not None:
-            data = self.apply_fill(data, self.fill, self.out_name, nan_fill)
+        data_fill = self.get_fill(data_var)
+        data = self.apply_fill(data, data_fill, out_name, nan_fill)
 
         # add units, make exception for units that are not defined in pint.
         if data_var.units not in ["degrees_north", "degrees_east",
@@ -189,7 +197,7 @@ class ReanalysisConversion:
             #  Special case: set snow depth missing values to 0 matching CFSR.
             data[data == fill_value] = 0.0
         if nan_fill:
-            data[data == fill_value] = np.nan   # no effect on masked arrays.
+            data[data == fill_value] = np.nan  # no effect on masked arrays.
         else:
             pass  # default from old code and this matters for extrapolation below surface.
 
@@ -203,7 +211,33 @@ class ReanalysisConversion:
     @property
     def long_name(self):
         """Update long_name from input name if function changes output."""
-        raise NotImplementedError
+        return self.long_name
+
+    def add_dependent(self, data_arr, secondary_name, secondary_nan_fill,
+                      secondary_time_ind, unmask=False):
+        """Add Dependent datasets needed for variable derivation.
+           These variables have traditionally been masked.
+
+           :param data_arr:  Netcdf4 Variable array
+           after creation of Quantity.  This is annoying.
+           :param secondary_name:  Name of the dependent variable
+           :param secondary_nan_fill:  The nan_fill flag for the dependent variable
+           :param secondary_time_ind:  The time index for the dependent variable
+           :param unmask:  boolean for the dependent variable mask.  Typically False,
+           unmask in some cases because mixed fill values in quantities create surprises.
+        """
+
+        support_data = self._get_data(data_arr, secondary_name, unmask, secondary_nan_fill,
+                                      secondary_time_ind, toa2sfc=False)
+
+        added_dependent = {secondary_name: support_data}
+
+        if self.dependent:
+            # Note:  Keep this in two steps, otherwise self.dependent returns None
+            self.dependent.update(added_dependent)
+            self.updateAttr("dependent", self.dependent)
+        else:
+            self.updateAttr("dependent", added_dependent)
 
     def _modify_shape(self):
         """Modify shape based on output characteristics."""
@@ -228,16 +262,19 @@ class ReanalysisConversion:
         """
         data = self.data
         ndims_out = len(self.data.shape)
+
         if ndims_out in [2, 3]:
             data = self._shift_lon()
 
-        if ndims_out == 3:
+        if ndims_out in [1, 3]:
             # do extrapolation before reshape
             # (extrapolate fn depends on a certain dimensionality/ordering)
-            data = self._extrapolate_below_sfc(data, fill)
-            data = np.swapaxes(data, 0, 2)
-            data = np.swapaxes(data, 0, 1)
-            data = data[:, :, ::-1]  # clavr-x needs toa->surface not surface->toa
+            if ndims_out == 3:
+                data = self._extrapolate_below_sfc(data, fill)
+                data = np.swapaxes(data, 0, 2)
+                data = np.swapaxes(data, 0, 1)
+                data = data[:, :, ::-1]  # clavr-x needs toa->surface not surface->toa
+
         return data
 
     @staticmethod
@@ -246,7 +283,6 @@ class ReanalysisConversion:
         if data.dtype in (np.float32, np.float64):
             data[np.isnan(data)] = CLAVRX_FILL
             data[data == old_fill] = CLAVRX_FILL
-        print(data.max())
         return data
 
     @staticmethod
@@ -344,18 +380,10 @@ class ReanalysisConversion:
 
         if self.fill is not None:
             out_sds.setfillvalue(CLAVRX_FILL)
-        # if self.out_units is not None:
-        #     out_sds.units = self.out_units
-        # elif self.out_units in ("none", "None"):
-        #     out_sds.units = "1"
-        # else:
-        #     out_sds.units = self.get_units
-
-        #unit_desc = " in [{}]".format(out_sds.units)
 
         out_sds.source_data = f"{in_file_short_value}->{self.shortname}{out_sds.units}"
 
-        out_sds.long_name = self.long_name()
+        out_sds.long_name = self.long_name
         out_sds.endaccess()
 
     def set_dim_names(self, out_sds):

conversions/derived_variables.py
@@ -1,5 +1,3 @@
-from typing import Callable
-
 import numpy as np
 import pint
 from netCDF4 import Dataset
@@ -91,16 +89,19 @@ def vapor_pressure_approximation(qv, sfc_pressure, plevels):
     return vapor_pressure
 
 
-def qv_to_rh(specific_humidity, temp_k, levels: pint.Quantity, press_at_sfc=None):
+def qv_to_rh(specific_humidity, temp_k, in_levels: pint.Quantity, press_at_sfc=None):
     """Convert Specific Humidity [kg/kg] -> relative humidity [%]."""
     # See Petty Atmos. Thermo. 4.41 (p. 65), 8.1 (p. 140), 8.18 (p. 147)
-    levels = (levels.to(ureg.pascal))   # [hPa] -> [Pa] when necessary.
+    levels = (in_levels.to(ureg.pascal))  # [hPa] -> [Pa] when necessary.
+
+    # match fill values
+
 
     es_tot = total_saturation_pressure(temp_k)
 
     vapor_pressure = vapor_pressure_approximation(specific_humidity, press_at_sfc, levels)
 
-    relative_humidity = (vapor_pressure / es_tot * 100.0).magnitude  # relative humidity [%]
+    relative_humidity = (vapor_pressure / es_tot).magnitude * 100.0  # relative humidity [%]
     relative_humidity[relative_humidity > 100.0] = 100.0
 
     return relative_humidity
@@ -181,16 +182,16 @@ def hack_snow(data: np.ndarray, mask_sd: Dataset) -> np.ndarray:
     return data
 
 
-def apply_conversion(scale_func: Callable, data: np.ndarray, fill, p_levels=None) -> np.ndarray:
-    """Apply fill to converted data after function."""
-    converted = data.copy()
-
-    if scale_func == "total_ozone":
-        converted = total_ozone(data, fill, p_levels)
-    else:
-        converted = scale_func(converted)
-
-        if fill is not None:
-            converted[data == fill] = fill
-
-    return converted
+# def apply_conversion(scale_func: Callable, data: np.ndarray, fill, p_levels=None) -> np.ndarray:
+#     """Apply fill to converted data after function."""
+#     converted = data.copy()
+#
+#     if scale_func == "total_ozone":
+#         converted = total_ozone(data, fill, p_levels)
+#     else:
+#         converted = scale_func(converted)
+#
+#         if fill is not None:
+#             converted[data == fill] = fill
+#
+#     return converted

conversions/file_tools.py
@@ -1,7 +1,7 @@
 import datetime
 import functools
 import logging
-from typing import Dict, Iterator, KeysView, Tuple, Union
+from typing import Dict, Iterator, KeysView, List, Tuple, Union
 
 import numpy as np
 from netCDF4 import Dataset, num2date
@@ -16,6 +16,7 @@ np.seterr(all="ignore")
 LOG = logging.getLogger(__name__)
 
 OutVarDictType = Dict[str, Union[MerraConversion, str]]
+Complicated_DatasetTimes = Dict[str, List[Tuple[int, datetime.datetime]]]
 
 
 def get_input_data(merra_ds: Dict[str, Union[Dataset, SD]],
@@ -25,75 +26,55 @@ def get_input_data(merra_ds: Dict[str, Union[Dataset, SD]],
 
     for out_key, rsk in output_vars_dict.items():
         LOG.info("Get data from %s for %s", rsk["in_file"], rsk["shortname"])
+        filetag = merra_ds[rsk["in_file"]].getncattr("Filename")
         out_vars["data_object"] = MerraConversion(
-            nc_dataset=merra_ds[rsk["in_file"]],
-            shortname=rsk["shortname"],
+            data_array=merra_ds[rsk["in_file"]].variables[rsk["shortname"]],
+            file_tag=filetag,
             out_name=out_key,
             out_units=rsk["out_units"],
             ndims_out=rsk["ndims_out"],
-            not_masked=True,
+            unmask=True,
             nan_fill=rsk["nan_fill"],
             time_ind=time_index[rsk["in_file"]]
         )
         if "dependent" in rsk:
             for support_var_name in rsk["dependent"]:
                 sub_field = rsk["dependent"][support_var_name]
-                support_obj = MerraConversion(
-                    nc_dataset=merra_ds[sub_field["in_file"]],
-                    shortname=sub_field["shortname"],
-                    out_name=support_var_name,
-                    out_units=sub_field["out_units"],
-                    ndims_out=sub_field["ndims_out"],
-                    not_masked=False,  # load these as masked arrays.
-                    nan_fill=sub_field["nan_fill"],
-                    time_ind=time_index[sub_field["in_file"]]
-                )
-                try:
-                    out_vars["dependent"].update({support_var_name: support_obj})
-                except KeyError:
-                    out_vars["dependent"] = {support_var_name: support_obj}
+                var_name = sub_field["shortname"]
+                data_array = merra_ds[sub_field["in_file"]].variables[var_name]
+                out_vars["data_object"].add_dependent(data_array,
+                                                      support_var_name,
+                                                      sub_field["nan_fill"],
+                                                      time_index[sub_field["in_file"]],
+                                                      sub_field["unmask"])
 
         yield out_vars
 
 
-def write_output_variables(datasets: Dict[str, Dataset], out_fields: Iterator[Dict]) -> None:
+def write_output_variables(datasets: Dict[str, Dataset], out_fields: Iterator[Dict], source_name: str) -> None:
     """Calculate the final output and write to file."""
-    if "ana" in datasets.keys():
-        file_pressure_levels = datasets["ana"].variables["lev"]
-        global_attrs = datasets["ana"]
-    else:
-        file_pressure_levels = datasets["asm3d"].variables["lev"]
-        global_attrs = datasets["asm3d"]
-
-    try:
-        source_name = global_attrs.getncattr("Source")
-    except KeyError:
-        source_name = ""
-
-    pint_unit_levels = conversions.Q_(np.asarray(file_pressure_levels), file_pressure_levels.units)
-
     while True:
         try:
             current_var = next(out_fields)
             out_var = current_var["data_object"]
-            file_tag = out_var.nc_dataset.getncattr("Filename")
             out_key = out_var.out_name
             LOG.info(f"Processing {out_key}")
 
             match out_key:
                 case "rh":
-                    temp_k = current_var["dependent"]["masked_temp_k"].data
-                    new_data = derive_var.qv_to_rh(out_var.data, temp_k, pint_unit_levels)
+                    new_data = derive_var.qv_to_rh(out_var.data, out_var.dependent["masked_temp_k"],
+                                                   out_var.dependent["unit_levels"])
                     new_data[np.isnan(new_data)] = new_data.fill_value
-                    out_var.updateAttr('fill', new_data.fill_value)
-                    out_var.updateAttr("data", new_data)
+                    out_var.updateAttr("fill", new_data.fill_value)
+                    out_var.updateAttr("data", new_data.filled())
                     out_var.updateAttr("out_units", "%")
                 case "rh at sigma=0.995":
-                    temp_t10m = current_var["dependent"]["masked_temperature_at_sigma"].data
-                    ps_pa = current_var["dependent"]["surface_pressure_at_sigma"].data
+                    temp_t10m = out_var.dependent["masked_temperature_at_sigma"]
+                    ps_pa = out_var.dependent["surface_pressure_at_sigma"]
                     new_data = derive_var.rh_at_sigma(temp_t10m, ps_pa,
-                                                      out_var.fill, pint_unit_levels, out_var.data)
-                    out_var.updateAttr("fill", new_data.fill_value)
+                                                      out_var.fill, out_var.dependent["unit_levels"], out_var.data)
+
+                    new_data[np.isnan(new_data)] = out_var.fill
                     out_var.updateAttr("data", new_data.filled())
                     out_var.updateAttr("out_units", "%")
                 case "water equivalent snow depth":
@@ -104,7 +85,7 @@ def write_output_variables(datasets: Dict[str, Dataset], out_fields: Iterator[Di
                     out_var.updateAttr("fill", None)
                     out_var.updateAttr("data", land_mask.astype(np.int32))
                 case "total ozone":
-                    new_data = derive_var.total_ozone(out_var.data, out_var.fill, pint_unit_levels)
+                    new_data = derive_var.total_ozone(out_var.data, out_var.fill, out_var.dependent["unit_levels"])
                     out_var.updateAttr("data", new_data)
                 case "total precipitable water":
                     out_var.updateAttr("data", (out_var.data / 10.0))
@@ -121,7 +102,7 @@ def write_output_variables(datasets: Dict[str, Dataset], out_fields: Iterator[Di
                         except AttributeError as _e:
                             raise AttributeError(f"Can't convert {out_key}: {out_var.data} to {out_var.out_units}")
 
-            out_var.update_output(datasets, f"{source_name}({file_tag})->{out_key}")
+            out_var.update_output(datasets, f"{source_name}({out_var.file_tag})->{out_key}")
 
         except StopIteration:
             break
@@ -173,11 +154,8 @@ def write_global_attributes(out_sd: SD, info_attrs) -> None:
     out_sd.end()
 
 
-def get_common_time(datasets: Dict[str, Dataset], input_type="geosfp"):
+def get_common_time(datasets: Dict[str, Dataset], input_type="geosfp") -> List[Complicated_DatasetTimes, List]:
     """Enforce a 'common' start time among the input datasets."""
-    dataset_times: Dict[str, datetime.datetime]
-    time_set: Dict[str, Set(datetime.datetime)]
-
     dataset_times = dict()
     time_set = dict()
     keys = list(datasets.keys())
@@ -232,7 +210,7 @@ def get_common_time(datasets: Dict[str, Dataset], input_type="geosfp"):
 
 
 def get_time_index(file_keys: KeysView[str],
-                   file_times: Dict[str, Tuple[int, datetime.datetime]],
+                   file_times: Complicated_DatasetTimes,
                    current_time: datetime.datetime) -> Dict[str, int]:
     """Determine the time index from the input files based on the output time.
 
@@ -245,6 +223,6 @@ def get_time_index(file_keys: KeysView[str],
         if file_name_key in ["mask", "mask_file"]:
             time_index[file_name_key] = 0
         else:
-            time_index[file_name_key] = [i for (i, t) in file_times[file_name_key]
-                                         if t == current_time][0]
+            time_index[file_name_key] = [index for (index, time_at_index) in file_times[file_name_key]
+                                         if time_at_index == current_time][0]
     return time_index